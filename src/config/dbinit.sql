076fb812981790b1267444a2bb11a0a6c8fb9436
Aventuraweb <jaquelineorozcoaranzazu@gmail.com>
2024-09-20 18:35:03 -0500

    corección de insersión de datos

--- src/config/dbinit.sql
+++ src/config/dbinit.sql
@@ -5,36 +5,38 @@ CREATE TABLE IF NOT EXISTS usuarios (
     password VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS productos (
-    idproducto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_producto VARCHAR(100) NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL,
-    image_url VARCHAR(255) NOT NULL, 
-    idcategoria INT 
-);
-
 CREATE TABLE IF NOT EXISTS categorias (
     idcategoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     nombre_categoria VARCHAR(100) NOT NULL
 )
 
--- Insertar datos iniciales
+CREATE TABLE IF NOT EXISTS productos (
+    idproducto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre_producto VARCHAR(100) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    image_url VARCHAR(255) NOT NULL, 
+    idcategoria INT not NULL
+);
+
+-- Insertar datos iniciales de usuarios 
 INSERT INTO usuarios (nombre_usuario, correo, password) VALUES 
 ('jaqueline', 'jaqueline1@gmail.com', 'orozco20'); 
 
-
-INSERT INTO productos (nombre_producto, precio, image_url, idcategoria) VALUES 
-(idproducto: 1, nombre_producto: "Tres Cordilleras", precio: '6000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928312/3-cordilleras_jfngny.png", idcategoria: 1 ),
-(idproducto: 2, nombre_producto: "Stella artois", precio: '7000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928357/220812050604_stellaartois-adlatina_sjmlvu.jpg",  idcategoria: 1 ),
-(idproducto: 3, nombre_producto: "Aguila", precio: '4000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928362/AGUILA-BOTELLA-600x600_fqymnv.jpg",  idcategoria: 1), 
-(idproducto: 4, nombre_producto: "Papas", precio: '4000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928344/815CKByawWL._AC_UF894_1000_QL80__rjybat.jpg", idcategoria: 2),
-(idproducto: 5, nombre_producto: "Chocolatinas", precio: '5000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928318/450_1000_asqwvr.jpg", idcategoria: 2),
-(idproducto: 6, nombre_producto: "Bom-Bom-Gum", precio: '7000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928383/images_agyhff.jpg", idcategoria: 2),
-(idproducto: 7, nombre_producto: "Buchanan`s", precio: '110000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928412/unnamed_6_53de1a21-b4d5-4ee5-a488-a796896e9011_bm7ahw.webp", idcategoria: 3),
-(idproducto: 8, nombre_producto: "Ron", precio: '58000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928396/Imagenes-03-Licores-preferidos-Colombianos_dpirrn.jpg", idcategoria: 3),
-(idproducto: 9, nombre_producto: "Baylis", precio: '50000', image_url: "https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928368/depositphotos_118386876-stock-photo-bottles-of-assorted-hard-liquor_f4isno.webp", 3); 
-
+-- insertar datos iniciales de categoría 
 INSERT INTO categorias ( nombre_categoria ) VALUES
 ('cerveza'), 
 ('snacks'), 
-('licores')
+('licores'); 
+
+-- insertar datos iniciañles de productos 
+INSERT INTO productos (nombre_producto, precio, image_url, idcategoria) VALUES 
+('Tres Cordilleras', 6000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928312/3-cordilleras_jfngny.png', 1),
+('Stella artois', 7000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928357/220812050604_stellaartois-adlatina_sjmlvu.jpg', 1),
+('Aguila', 4000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928362/AGUILA-BOTELLA-600x600_fqymnv.jpg', 1), 
+('Papas', 4000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928344/815CKByawWL._AC_UF894_1000_QL80__rjybat.jpg', 2),
+('Chocolatinas', 5000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928318/450_1000_asqwvr.jpg', 2),
+('Bom-Bom-Gum', 7000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928383/images_agyhff.jpg', 2),
+('Buchanan`s', 110000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928412/unnamed_6_53de1a21-b4d5-4ee5-a488-a796896e9011_bm7ahw.webp', 3),
+('Ron', 58000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928396/Imagenes-03-Licores-preferidos-Colombianos_dpirrn.jpg', 3),
+('Baylis', 50000, 'https://res.cloudinary.com/dd3oucmlo/image/upload/v1725928368/depositphotos_118386876-stock-photo-bottles-of-assorted-hard-liquor_f4isno.webp', 3);
+
